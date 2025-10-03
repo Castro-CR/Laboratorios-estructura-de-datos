eb3be6f9aa48fff80924a353af0ad3948dececb4
Castro-CR <castroanthony486@gmail.com>
2025-10-02 21:12:14 -0600

    Primera parte Lab03

---
+++ Lab3/src/poo/Carro.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo;
+
+/**
+ *
+ * @author acastro
+ */
+public class Carro {
+    //Atributos marca, modelo, año y color
+    private String marca;
+    private String modelo;
+    private String color;
+    private int año;
+    private final String SIN_COLOR = "SIN COLOR";
+    private final int DEFAULT_YEAR = 2000;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public Carro(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = SIN_COLOR;
+        this.año = DEFAULT_YEAR;
+    }
+    
+    public Carro() {
+        this.marca ="INDIQUE MARCA";
+        this.modelo ="INDIQUE MODELO";
+        this.color = SIN_COLOR;
+        this.año = DEFAULT_YEAR;
+    }
+
+    public Carro(String marca, String modelo, String color, int año) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.año = año;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" + "marca=" + marca + ", modelo=" + modelo + ", color=" + color + ", a\u00f1o=" + año + '}';
+    }
+    
+    
+    
+}
+    
+    
+    
+ 
---
+++ Lab3/src/poo/Garaje.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo;
+
+/**
+ *
+ * @author acastro
+ */
+public class Garaje {
+    private final int capacidad;
+    private final Carro[] espacios;
+    private int index;
+
+    public Garaje(int capacidad) {
+        this.capacidad = capacidad;
+        this.espacios = new Carro[capacidad];
+    }
+    
+    //aparcarCarro
+    public void aparcarCarro(Carro carro){
+        if (index < capacidad) {
+            this.espacios[index++] = carro;
+            System.out.printf("Aparcando %s-%s%n",carro.getMarca(), carro.getModelo());
+        }else {
+            System.err.printf("Garaje Lleno!!, capacidad maxima de %d\n", this.capacidad);
+        }
+    }
+    //listar carros()
+    public void listar(){
+        for (Carro espacio : espacios) {
+            System.out.printf(" %s[%s]\n", espacio.getMarca(), espacio.getModelo());
+        }
+    }
+}

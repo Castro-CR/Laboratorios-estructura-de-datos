c076df6bbd8694d22b9b5ea25a26e12be7ea2c87
Castro-CR <castroanthony486@gmail.com>
2025-10-09 13:32:57 -0600

    Parte 3 de lab 3

--- Lab3/src/lab3/Lab3.java
+++ Lab3/src/lab3/Lab3.java
@@ -31,14 +31,34 @@ public class Lab3 {
         c2.setAño(2020); 
         System.out.println(c2.toString());
         
+        Carro c3 = new Carro("Ford","Mustang","Blanco",1990);
+        c3.setAño(2020);
+        System.out.println(c3.toString());
+        
+        Carro c4 = new Carro("Chevrolet","Camaro","Negro",2025);
+        c4.setAño(2020);
+        System.out.println(c4.toString());
+        
+        Carro c5 = new Carro("Hyundai","Elantra","Naranja",2019);
+        c5.setAño(2020);
+        System.out.println(c5.toString());
+        
+        Carro c6 = new Carro("BMW","M4 COMPETION","Gris",2020);
+        c6.setAño(2020);
+        System.out.println(c6.toString());
+        
         Garaje miGaraje = new Garaje (2);
         miGaraje.aparcarCarro(c1);
         miGaraje.aparcarCarro(c2);
-        miGaraje.aparcarCarro(c1);
+        miGaraje.aparcarCarro(c3);
         
         Carretera c = new Carretera("General Cañas");
         c.agregarCarro(c1);
         c.agregarCarro(c2);
+        c.agregarCarro(c3);
+        c.agregarCarro(c4);
+        c.agregarCarro(c5);
+        c.agregarCarro(c6);
         c.listarCarros();
 
     }
--- Lab3/src/poo/Carretera.java
+++ Lab3/src/poo/Carretera.java
@@ -23,6 +23,7 @@ public class Carretera {
      //agregarCarro
     public void agregarCarro(Carro carro) {
         this.carros.add(carro);
+        this.verificarEstado();
     }
     //cantidaddeCarros
     public int cantidadDeCarros() {
@@ -34,6 +35,19 @@ public class Carretera {
         if (cantidad < 3) {
             System.out.printf("La carretera %s tiene un flujo ligero.\n", this.nombre);
     }
+        else if (cantidad < 4) {
+            System.out.printf("La carretera %s tiene un flujo moderado.\n", this.nombre);
+        }
+        else if (cantidad < 5) {
+            System.out.printf("La carretera %s está comenzando a congestionarse.\n", this.nombre);
+        }
+        else  if (cantidad < 6) {
+            System.out.printf("Se está generando un embotellamiento en la carretera %s.\n", this.nombre);
+        
+        }
+        else if(cantidad < 7) {
+            System.out.printf("La carretera %s está colapsada, hay mucho embotellamiento.\n", this.nombre);
+        }
         //switch
     }
     //listarCarros
@@ -51,9 +65,12 @@ public class Carretera {
         var it = this.carros.iterator();
         while (it.hasNext()) {        
             Carro next = it.next();
+            int año = 2025;
             //validar antiguedad del carro.
-            this.carros.remove(it);
-            System.out.printf(" %s[%s]\n", next.getMarca(), next.getModelo());
+            if (año > 20){
+             System.out.printf(" %s[%s]\n", next.getMarca(), next.getModelo());
+            it.remove();
+            }
         }
     }
 }
